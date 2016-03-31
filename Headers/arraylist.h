cd020274cf59bca83e34e3de98ff97855fbfd1f0
Nathan <nschneid95@gmail.com>
2016-03-31 15:29:05 -0500

    ArrayList Works Tested the arraylist code and fixed some bugs

--- Headers/arraylist.h
+++ Headers/arraylist.h
@@ -18,22 +18,22 @@ typedef struct{
 /*
  * Initializes a new array list and returns a pointer to it
  */
-arraylist_t* ArrayList_InitArrayList();
+arraylist_t* arraylist_InitArraylist();
 /*
  * Adds the cell to the arraylist at the given position
  * Returns 0 on success and -1 on invalid arguments
  */
-int arraylist_Add(cell_t cell, int index, arraylist_t arraylist);
+int arraylist_Add(cell_t cell, int index, arraylist_t* arraylist);
 /*
  * Fills the cell pointer with the cell at the index in the arraylist
  * Returns 0 on success and -1 on invalid arguments
  */
-int arraylist_Get(int index, arraylist_t arraylist, cell_t* cell);
+int arraylist_Get(int index, arraylist_t* arraylist, cell_t* cell);
 /*
  * Removes the cell at the given position from the arraylist and puts it in the cell pointer
  * Returns 0 on success and -1 on invalid arguments
  * If a null pointer is passed in then simply removes the cell
  */
-int arraylist_Remove(int index, arraylist_t arraylist, cell_t* cell);
+int arraylist_Remove(int index, arraylist_t* arraylist, cell_t* cell);
 
 #endif /* ARRAYLIST_H_ */
--- Sources/arraylist.c
+++ Sources/arraylist.c
@@ -8,30 +8,30 @@
  * Increases the size of the array
  * Returns 0 on success and -1 on invalid arguments
  */
-int ArrayList_IncreaseSize(arraylist_t* arraylist);
+int arraylist_IncreaseSize(arraylist_t* arraylist);
 
 /*
  * Initializes a new array list and returns a pointer to it
  */
-arraylist_t* ArrayList_InitArrayList() {
+arraylist_t* arraylist_InitArraylist() {
 	arraylist_t* arraylist = (arraylist_t*)malloc(sizeof(arraylist_t));
 	arraylist->size = ARRAYLIST_INITIALSIZE;
 	arraylist->numCells = 0;
-	arraylist->array = (cell_t)malloc(sizeof(cell_t) * arraylist->size);
+	arraylist->array = (cell_t*)malloc(sizeof(cell_t) * arraylist->size);
 	return arraylist;
 }
 /*
  * Adds the cell to the arraylist at the given position
  * Returns 0 on success and -1 on invalid arguments
  */
-int arraylist_Add(cell_t cell, int index, arraylist_t arraylist) {
+int arraylist_Add(cell_t cell, int index, arraylist_t* arraylist) {
 	if(arraylist == 0) return -1; //Check if null pointer
-	if(index < 0 || index >= arraylist->numCells) return -1;
+	if(index < 0 || index > arraylist->numCells) return -1;
 	//Note: cell is local so it cannot be null
 	if(arraylist->numCells == arraylist->size) arraylist_IncreaseSize(arraylist); //The array is full
 	int i;
 	for(i = arraylist->numCells; i >= index; i--) {
-		arraylist->array[i+1] = arraylist[i];
+		arraylist->array[i+1] = arraylist->array[i];
 	}
 	arraylist->array[index] = cell;
 	arraylist->numCells++;
@@ -42,7 +42,7 @@ int arraylist_Add(cell_t cell, int index, arraylist_t arraylist) {
  * Fills the cell pointer with the cell at the index in the arraylist
  * Returns 0 on success and -1 on invalid arguments
  */
-int arraylist_Get(int index, arraylist_t arraylist, cell_t* cell) {
+int arraylist_Get(int index, arraylist_t* arraylist, cell_t* cell) {
 	if(arraylist == 0 || cell == 0) return -1; //Check if null pointers
 	if(index < 0 || index >= arraylist->numCells) return -1;
 	*cell = arraylist->array[index];
@@ -52,19 +52,19 @@ int arraylist_Get(int index, arraylist_t arraylist, cell_t* cell) {
  * Removes the cell at the given position from the arraylist and puts it in the cell pointer
  * Returns 0 on success and -1 on invalid arguments
  */
-int arraylist_Remove(int index, arraylist_t arraylist, cell_t* cell) {
+int arraylist_Remove(int index, arraylist_t* arraylist, cell_t* cell) {
 	if(arraylist == 0) return -1; //Check if null pointers
 	if(index < 0 || index >= arraylist->numCells) return -1;
 	if(cell != 0) *cell = arraylist->array[index]; //Copy the element to be deleted
 	int i;
-	for(i = index; i < arraylist.numCells - 1; i++) {
+	for(i = index; i < arraylist->numCells - 1; i++) {
 		arraylist->array[i] = arraylist->array[i+1];
 	}
-	arraylist.numCells--;
+	arraylist->numCells--;
 	return 0;
 }
 
-int arraylist_IncreaseSize(arraylist_t arraylist) {
+int arraylist_IncreaseSize(arraylist_t* arraylist) {
 	if(arraylist == 0) return -1;
 	cell_t* newArray = (cell_t*)malloc(arraylist->size * 2 * sizeof(cell_t));
 	int i;
@@ -73,5 +73,6 @@ int arraylist_IncreaseSize(arraylist_t arraylist) {
 	}
 	free(arraylist->array);
 	arraylist->array = newArray;
+	arraylist->size *= 2;
 	return 0;
 }
